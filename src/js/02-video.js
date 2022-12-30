255b985f9cee9bd26a03e1f9bf7d190e333e0408
Maksym Stankov <maksym.stankov@gmail.com>
2022-12-30 14:55:05 +0200

    task #1, #2, #3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,31 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+const STORAGEDATA_KEY = 'videoplayer-current-time';
+
+function currentTime(data) {
+  localStorage.setItem(STORAGEDATA_KEY, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+const savedData = localStorage.getItem(STORAGEDATA_KEY) || [];
+const seconds = JSON.parse(savedData);
+
+player
+  .setCurrentTime(seconds)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const CURRENTTEXT_KEY = 'feedback-form-state';
+function onInputForm() {
+  const savedMessage = formEl.message.value;
+  const savedEmail = formEl.email.value;
+  localStorage.setItem(
+    CURRENTTEXT_KEY,
+    JSON.stringify({ email: savedEmail, message: savedMessage })
+  );
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(CURRENTTEXT_KEY);
+}
+
+function inputAfterReload() {
+  const savedData = localStorage.getItem(CURRENTTEXT_KEY);
+  const parseData = JSON.parse(savedData) || [];
+  console.log(parseData);
+  if (parseData.message) {
+    formEl.message.value = parseData.message;
+  }
+
+  if (parseData.email) {
+    formEl.email.value = parseData.email;
+  }
+}
+
+formEl.addEventListener('submit', onFormSubmit);
+
+formEl.addEventListener('input', throttle(onInputForm, 500));
+
+inputAfterReload();
